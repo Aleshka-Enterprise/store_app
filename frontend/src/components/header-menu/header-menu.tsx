1857a11e85a7af22f0857942786d3b3b11f29cf6
Aleshka-Enterprise <Sleska4@yandex.ru>
2023-10-04 13:32:10 +0300

    Доработка хэдера.

--- frontend/src/components/header-menu/header-menu.tsx
+++ frontend/src/components/header-menu/header-menu.tsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+import 'font-awesome/css/font-awesome.min.css';
 import "./header-menu.scss"
 
 const HeaderMenu = (): React.ReactElement => {
+  const [isMenuDisplayed, setMenuDisplayed] = useState<boolean>(false);
   const navigate = useNavigate();
+
+  const dropdownMenu = (): React.ReactElement => {
+    return (
+      <div className='dropdown-menu'>
+        <li>Профиль</li>
+        <li>Заказы</li>
+        <li>Админ-панель</li>
+        <hr />
+        <li>Выйти</li>
+      </div>
+    );
+  };
 
   return (
     <div className='header-menu'>
       <div className='container'>
         <div className='logo' onClick={(): void => navigate("/")}>Store</div>
+        <div className='navbar-nav'>
+          <div className='elem' onClick={(): void => navigate("/products/")}>
+            Каталог
+            <i className="fa fa-shopping-bag"></i>
+          </div>
+          <div className='elem' onClick={(): void => navigate("/autorization/")}>
+            Войти
+            <i className="fa fa-sign-in" aria-hidden="true"></i>
+          </div>
+          <div style={{ position: 'relative' }} onClick={(): void => setMenuDisplayed(!isMenuDisplayed)}>
+            <i className='fa fa-user-circle elem' aria-hidden="true"></i>
+            {isMenuDisplayed && dropdownMenu()}
+          </div>
+        </div>
       </div>
     </div>
   );
